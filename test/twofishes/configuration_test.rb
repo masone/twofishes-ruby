0c67252c5208da90aadc6c88f669997955c8bdc2
Christian Felder (masone) <ema@rh-productions.ch>
2014-06-19 14:56:02 +0200

    fixes tests

--- test/twofishes/configuration_test.rb
+++ test/twofishes/configuration_test.rb
@@ -16,12 +16,22 @@ describe Twofishes::Configuration do
     assert_equal 'http://localhost:8081', configuration.base_url
   end
 
+  it "should reset configuration" do
+    Twofishes.configure do |config|
+      config.base_url = 'http://demo.twofishes.net'
+    end
+    Twofishes.reset_configuration
+
+    assert_equal 'http://localhost:8081', configuration.base_url
+  end
+
   it "should configure base_url" do
     Twofishes.configure do |config|
       config.base_url = 'http://demo.twofishes.net'
     end
 
     assert_equal 'http://demo.twofishes.net', configuration.base_url
+    Twofishes.reset_configuration
   end
 
 end
