236f6b90533d9841d163ae68bf7b2371510f215b
Christian Felder (masone) <ema@rh-productions.ch>
2014-07-31 00:43:15 +0200

    clears fakeweb registry for each test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,3 +9,11 @@ require 'mocha/mini_test'
 require 'fakeweb'
 
 FakeWeb.allow_net_connect = /^https:\/\/codeclimate\.com/
+
+class MiniTest::Spec
+
+  before do
+    FakeWeb.clean_registry
+  end
+
+end
