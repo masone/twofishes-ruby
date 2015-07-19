340a7577a047f2794b921f7abadc75e9dc11df5c
Christian Felder (masone) <ema@rh-productions.ch>
2015-07-19 11:45:41 +0200

    lower complexity of prepare_bounds and prepare_ll

--- lib/twofishes/geocode_request.rb
+++ lib/twofishes/geocode_request.rb
@@ -13,11 +13,11 @@ module Twofishes
       case ll
       when String
         lat, lng = ll.split(/\s*,\s*/)
-        GeocodePoint.new(lat: lat.to_f, lng: lng.to_f)
+        new_point(lat, lng)
       when Array
-        GeocodePoint.new(lat: ll[0].to_f, lng: ll[1].to_f)
+        new_point(ll[0], ll[1])
       when Hash
-        GeocodePoint.new(lat: ll[:lat].to_f, lng: ll[:lng].to_f)
+        new_point(ll[:lat], ll[:lng])
       else
         ll
       end
@@ -27,17 +27,11 @@ module Twofishes
       case bounds
       when String
         ne_lat, ne_lng, sw_lat, sw_lng = bounds.split(/\s*,\s*/)
-        ne = GeocodePoint.new(lat: ne_lat.to_f, lng: ne_lng.to_f)
-        sw = GeocodePoint.new(lat: sw_lat.to_f, lng: sw_lng.to_f)
-        GeocodeBoundingBox.new(ne: ne, sw: sw)
+        new_bounding_box(ne_lat, ne_lng, sw_lat, sw_lng)
       when Array
-        ne = GeocodePoint.new(lat: bounds[0].to_f, lng: bounds[1].to_f)
-        sw = GeocodePoint.new(lat: bounds[2].to_f, lng: bounds[3].to_f)
-        GeocodeBoundingBox.new(ne: ne, sw: sw)
+        new_bounding_box(bounds[0], bounds[1], bounds[2], bounds[3])
       when Hash
-        ne = GeocodePoint.new(lat: bounds[:ne_lat].to_f, lng: bounds[:ne_lng].to_f)
-        sw = GeocodePoint.new(lat: bounds[:sw_lat].to_f, lng: bounds[:sw_lng].to_f)
-        GeocodeBoundingBox.new(ne: ne, sw: sw)
+        new_bounding_box(bounds[:ne_lat], bounds[:ne_lng], bounds[:sw_lat], bounds[:sw_lng])
       else
         bounds
       end
@@ -63,6 +57,18 @@ module Twofishes
       end
 
       options
+    end
+
+    private
+
+    def new_bounding_box(ne_lat, ne_lng, sw_lat, sw_lng)
+      ne = GeocodePoint.new(lat: ne_lat.to_f, lng: ne_lng.to_f)
+      sw = GeocodePoint.new(lat: sw_lat.to_f, lng: sw_lng.to_f)
+      GeocodeBoundingBox.new(ne: ne, sw: sw)
+    end
+
+    def new_point(lat, lng)
+      GeocodePoint.new(lat: lat.to_f, lng: lng.to_f)
     end
   end
 end
